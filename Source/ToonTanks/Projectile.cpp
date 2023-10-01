e54be81b0ee9339fd828ff890447fe5ce2130259
andysharky <heckermanacc@gmail.com>
2023-10-01 10:46:53 +0300

    message

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -9,7 +9,7 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	ProjectileMesh = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Projectile Mesh"));
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
 	RootComponent = ProjectileMesh;
 
 }
