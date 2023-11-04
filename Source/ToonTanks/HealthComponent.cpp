1003e5aad2f9683b0ecdee8f03a5af3f267dff31
andysharky <heckermanacc@gmail.com>
2023-11-04 09:19:41 +0200

    Backup

--- Source/ToonTanks/HealthComponent.cpp
+++ Source/ToonTanks/HealthComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "HealthComponent.h"
+#include "GameFramework/Actor.h"
+
 
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
@@ -19,16 +21,20 @@ void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	Health = MaxHealth;
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
 	
 }
-
-
 // Called every frame
 void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+}
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser) 
+{
+	if(Damage <= 0.f) return;
+	
+	Health -= Damage;
+	UE_LOG(LogTemp, Warning, TEXT("Health: %f"), Health);
 }
 
--- Source/ToonTanks/HealthComponent.h
+++ Source/ToonTanks/HealthComponent.h
@@ -19,6 +19,14 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxHealth = 100.f;
+	float Health = 0.f;
+
+	UFUNCTION()
+	void DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser);
+	
 
 public:	
 	// Called every frame
